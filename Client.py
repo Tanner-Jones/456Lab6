3b1152d71d1473f535fc2b6ef1875d5f372f6df3
Tanner-Jones <56734340+Tanner-Jones@users.noreply.github.com>
2021-03-24 13:39:42 -0600

    Add files via upload

---
+++ Client.py
@@ -0,0 +1,39 @@
+import socket
+import sys
+
+# collect all command line arguments
+Server_name = sys.argv[1]
+Server_port = sys.argv[2]
+Execution_count = sys.argv[3]
+Time_delay = sys.argv[4]
+command = sys.argv[5]
+
+# create a few necessary variables for rest of program
+HOSTNAME = socket.gethostbyname(Server_name)
+message = "Execution Count:" + Execution_count + "Time delay:" + Time_delay + "Command:" + command
+message = bytes(message)
+
+def TCP_connection():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((HOSTNAME, Server_port))
+    sock.sendall(message)
+
+
+def UDP_connection():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.sendto(message, (HOSTNAME, Server_port))
+
+
+
+
+conn_type = input("TCP or UDP?")
+if conn_type == "TCP":
+    TCP_connection()
+elif conn_type == "UDP":
+    UDP_connection()
+else:
+    print("Invalid input, please try again")
+
+
+
+
---
+++ Server.py
@@ -0,0 +1,42 @@
+import socket
+import time
+
+
+# Ports and IP initialization
+IP = socket.gethostname()
+TCP_PORT_RECEIVE = 5005
+TCP_PORT_SEND = 4444
+UDP_PORT_RECEIVE = 5006
+UDP_PORT_SEND = 4445
+
+#creates socket object for TCP
+TCPsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+TCPsock.bind((IP, TCP_PORT_RECEIVE))
+
+# Creates socket for UDP
+UDPsock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+UDPsock.bind((IP, UDP_PORT_RECEIVE))
+
+
+while True:
+    #Loop to wait for received messages
+    TCPsock.listen()
+    UDPsock.listen()
+    TCPconn, TCPaddr = TCPsock.accept()
+    UDPconn, UDPaddr = TCPsock.accept()
+    print("Waiting for Connection:")
+    TCPdata = UDPconn.recv(1024)
+    UDPdata = UDPconn.recv(1024)
+    print(UDPdata)
+    print(TCPdata)
+    # prints message to ask user about acceptance
+
+
+
+
+
+
+
+
+
+
