fdb8a7eafe48b860f31cf2f7c5ab8657a12c77df
Tanner-Jones <56734340+Tanner-Jones@users.noreply.github.com>
2021-03-25 14:12:22 -0600

    Add files via upload

--- Server.py
+++ Server.py
@@ -1,6 +1,7 @@
 import socket
 import time
 import subprocess
+import sys
 
 # Ports and IP initialization
 IP = socket.gethostname()
@@ -9,22 +10,30 @@ TCP_PORT_RECEIVE = 5005
 TCP_PORT_SEND = 4444
 UDP_PORT_RECEIVE = 5006
 UDP_PORT_SEND = 4445
+PORT = sys.argv[1]
 
 #creates socket object for TCP
 TCPsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-TCPsock.bind((IP, TCP_PORT_RECEIVE))
+TCPsock.bind((IP, PORT))
 
 # Creates socket for UDP
 UDPsock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-UDPsock.bind((IP, UDP_PORT_RECEIVE))
+UDPsock.bind((IP, PORT))
+
+# output file to write to
+f = open("output", 'wb')
+
 
 def run_command(count, delay, command):
     for i in range(0,int(count)):
         ran = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         output, err = ran.communicate()
-        print('It is', output)
+        output_s = output.decode("utf-8")
+        print(time.strftime('%X %x'), output_s)
+        output_s = output_s + time.strftime('%X %x')
+        f.write(output_s)
         time.sleep(int(delay))
-
+    f.close()
 
 def get_details(message):
     message = message.replace("Execution Count:", '')
@@ -38,7 +47,6 @@ def get_details(message):
     return count, delay, command
 
 def UDP_server():
-    #Loop to wait for received messages
     print("Waiting for Connection:")
     UDPdata, UDPaddr = UDPsock.recvfrom(1024)
     message = UDPdata.decode("utf-8")
@@ -47,7 +55,6 @@ def UDP_server():
     # prints message to ask user about acceptance
 
 def TCP_server():
-    #Loop to wait for received messages
     TCPsock.listen(5)
     TCPconn, TCPaddr = TCPsock.accept()
     print("Waiting for Connection:")
@@ -55,7 +62,11 @@ def TCP_server():
     message = TCPdata.decode("utf-8")
     count, delay, command = get_details(message)
     run_command(count, delay, command)
+    f = open("output," 'rb')
+    send_message = f.read()
+    TCPconn.sendall(send_message)
     # prints message to ask user about acceptance
+
 
 while True:
     server_type = input("UDP Server or TCP Server? UDP/TCP\n")
