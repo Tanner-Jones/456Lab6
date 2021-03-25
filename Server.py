a0f314062fa00d6c4f2a10ebd281052d67e68736
Tanner-Jones <56734340+Tanner-Jones@users.noreply.github.com>
2021-03-25 15:06:07 -0600

    Add files via upload

--- Server.py
+++ Server.py
@@ -25,12 +25,18 @@ f = open("output", 'w')
 
 
 def run_command(count, delay, command):
+
     for i in range(0,int(count)):
+        if server_type == "TCP":
+            TCPconn, TCPaddr = TCPsock.accept()
+            check = TCPconn.recv(1024)
+            if check.decode('utf-8') == "rcend":
+                break
         ran = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         output, err = ran.communicate()
         output_s = output.decode("utf-8")
         print(time.strftime('%X %x'), output_s)
-        output_s = output_s + time.strftime('%X %x')
+        output_s = output_s + time.strftime('%X %x') + "\n"
         f.write(output_s)
         time.sleep(int(delay))
     f.close()
@@ -52,20 +58,33 @@ def UDP_server():
     message = UDPdata.decode("utf-8")
     count, delay, command = get_details(message)
     run_command(count, delay, command)
+
+    f = open("output", 'rb')
+    send_message = f.read()
+
+    UDPsock.close()
+    UDPsock_send = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    UDPsock_send.sendto(send_message, (UDPaddr, PORT))
     # prints message to ask user about acceptance
 
-def TCP_server():
+def TCP_server(TCPconn):
+    # block sets up socket and receives command
     TCPsock.listen(5)
     TCPconn, TCPaddr = TCPsock.accept()
     print("Waiting for Connection:")
     TCPdata = TCPconn.recv(1024)
     message = TCPdata.decode("utf-8")
+    TCPconn.close()
+    # collects the three relevant details from message and runs the command
     count, delay, command = get_details(message)
     run_command(count, delay, command)
+
+    # reads output and sends back to client
     f = open("output", 'rb')
     send_message = f.read()
     TCPconn.sendall(send_message)
     # prints message to ask user about acceptance
+    TCPsock.close()
 
 
 while True:
